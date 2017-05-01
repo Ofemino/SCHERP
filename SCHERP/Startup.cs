ddc793aa55b87be775f4ef2a1ec114b3b24c19ab
Ofemino <ofemino@gmail.com>
2017-05-01 21:58:54 +0100

    Add project files.

---
+++ SCHERP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SCHERP.Startup))]
+namespace SCHERP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
